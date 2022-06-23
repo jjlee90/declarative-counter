fee3a2e23dac9fd8dc2ea5849112826e25b0a54b
Joseph Lee <josephlee@Josephs-MBP.fios-router.home>
2022-06-22 21:45:03 -0400

    counter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+export default function App() {
+  let [counter, changeCounter] = useState(1);
+  window.changeCounter = changeCounter;
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
+  );
+}
